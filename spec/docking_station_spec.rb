fd1ad18579f32fabf2956a463dcf0dd9c4bccf67
David James <d.james88@btinternet.com>
2015-05-21 18:17:04 +0100

    docking station capacity

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,13 @@
-require "docking_station"
+require_relative '../lib/docking_station'
 
 describe DockingStation do
 
-  it 'should be able to accept a bike' do
-    station = DockingStation.new
-    bike = Bike.new 
+  let(:station) { DockingStation.new(:capacity => 20) } 
+  let(:bike) { Bike.new }
+  
+  it 'should be able to accept a bike' do 
+    
+    bike = Bike.new
 
     expect(station.bike_count).to eq 0
 
@@ -12,5 +15,22 @@ describe DockingStation do
 
     expect(station.bike_count).to eq 1
   end
-  
+
+
+  it "should release a bike" do
+     station.dock(bike)
+     station.release(bike)
+     expect(station.bike_count).to eq 0
+  end
+
+  it "should know when it's full" do
+    expect(station.full?).not_to be true
+    20.times { station.dock(Bike.new) }
+    expect(station.full?).to be true
+  end
+
+  it "should not accept a bike if it's full" do
+   20.times { station.dock(Bike.new) }
+   expect { station.dock(bike) }.to raise_error RuntimeError
+  end
 end
