fb5d4448ae54c3c19e6d256d78a76e9661bea997
David James <d.james88@btinternet.com>
2015-05-21 16:32:46 +0100

    refectored bike creation using let

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,16 +2,21 @@
 require_relative '../lib/bike'
 
 describe Bike do
+  let(:bike) { Bike.new }
   #this is a specific feature or behaviour
   #that we expect to have
   it "should not be broken when created" do
-    bike = Bike.new  #instaniate a new instance of the Bike class
     expect(bike.broken?).to be false
   end
 
   it "should be able to break" do
-    bike = Bike.new
     bike.break
     expect(bike.broken?).to be true 
+  end
+
+  it "should be able to be fixed" do
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be false
   end
 end
